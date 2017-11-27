eacdfccfc4659955de96575b8940eb4e7e7a8eb2
ruinering <ruine@bk.ru>
2017-11-27 16:24:43 +0300

    Added tests for polls

--- lab01/polls/tests.py
+++ lab01/polls/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+class Tests(TestCase):
+	def setUp(self):
+		self.client = Client()
+
+	def testok(self):
+		response = self.client.get('/index.html')
+		self.assertEqual(response.status_code, 200)
+		response = self.client.get('')
+		self.assertEqual(response.status_code, 200)
+
+	def testnotfound(self):
+		response = self.client.get('/a.html')
+		self.assertEqual(response.status_code, 404)
