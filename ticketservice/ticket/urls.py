bfaf1b239966a10402c40f031bde27ae0bb761f1
ruinering <ruine@bk.ru>
2017-12-05 20:09:29 +0300

    Removed other projects for deploy on Heroku

--- ticketservice/ticket/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from ticket import views
-
-urlpatterns = [
-    url(r'^ticket/$', views.TicketList.as_view(), name='ticket-list'),
-    url(r'^ticket/(?P<pk>[0-9]+)/$', views.TicketDetail.as_view(), name='ticket-detail'),
-]
--- userservice/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from user import views
-
-urlpatterns = [
-    url(r'^user/$', views.UserList.as_view(), name='user-list'),
-    url(r'^user/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
-]
